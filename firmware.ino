e0fe24ef19d69b142d1eeadd5c941f3efb14c34d
Vincent Rodley <vindognz@gmail.com>
2026-06-06 11:04:04 +1200

    Session #2: Firmware skeleton

---
+++ firmware.ino
@@ -0,0 +1,13 @@
+const int LED_PIN = PB1;
+const int BUTTON_PIN = PB2;
+const int BRIGHTNESS_PIN = PB3;
+
+void setup() {
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    pinMode(BRIGHTNESS_PIN, INPUT)
+}
+
+void loop() {
+    // TODO
+}
