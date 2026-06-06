0e31717077e687ccb00fca825c565d55ed6658d4
Vincent Rodley <vindognz@gmail.com>
2026-06-06 12:38:05 +1200

    Session #2: add button debouncing with 50ms delay

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -2,18 +2,42 @@ const int LED_PIN = PB1;
 const int BUTTON_PIN = PB2;
 const int BRIGHTNESS_PIN = PB3;
 
+// debounce variables
+unsigned long lastDebounceTime = 0;
+const unsigned debounceDelay = 50; // 50ms debounce
+
 void setup() {
+    // initialize everything :)
     pinMode(LED_PIN, OUTPUT);
     pinMode(BUTTON_PIN, INPUT_PULLUP);
     pinMode(BRIGHTNESS_PIN, INPUT);
-    digitalWrite(LED_PIN, LOW); // initialize the LED off
+    digitalWrite(LED_PIN, LOW); // initialize the LED low
 }
 
 void loop() {
-    // read the button (input_pullup: low = presed, high = not pressed)
-    if (digitalRead(BUTTON_PIN) == LOW) {
-        digitalWrite(LED_PIN, HIGH); // button pressed = led on
+    static bool lastReading = HIGH;
+    static bool stableButtonState = HIGH;
+
+    bool reading = digitalRead(BUTTON_PIN);
+
+    // if reading changed, reset debounce timer
+    if (reading != lastReading) {
+        lastDebounceTime = millis();
+    }
+
+    // after debounce delay, update stable state
+    if ((millis() - lastDebounceTime) > debounceDelay) {
+        if (reading != stableButtonState) {
+            stableButtonState = reading;
+        }
+    }
+
+    lastReading = reading;
+
+    // update LED to debounced button state
+    if (stableButtonState == LOW) {
+        digitalWrite(LED_PIN, HIGH); // button pressed
     } else {
-        digitalWrite(LED_PIN, LOW); // button released = led off
+        digitalWrite(LED_PIN, LOW); // button released
     }
 }
